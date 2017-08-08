e3f4391d06cbae7159caa457897608e56f96c3e4
Prabhjot Singh <prabh.me@outlook.com>
2017-08-08 14:09:48 +0530

    Update firebase_cloud_function.js

--- firebase_cloud_function.js
+++ firebase_cloud_function.js
@@ -9,13 +9,15 @@ exports.add_to_cart = functions.analytics.event('add_to_cart').onLog(event => {
 		var http = require("http");
 		var needle = require('needle');
 		const email_id = event.data.params.email_id;
+		const product_group_id = event.data.params.product_group_id;
 		const logTime = event.data.logTime;
+		const email_and_product_group_id = email_id+','+product_group_id;
 
 var options = {
 	headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 };
 
-needle.post('<YOUR SERVER DNS/IP>/facebook_add_audience.php', email_id, options, function(err, resp) {
+needle.post('<YOUR SERVER DNS/IP>/facebook_add_audience.php', email_and_product_group_id , options, function(err, resp) {
 });
 
 });
